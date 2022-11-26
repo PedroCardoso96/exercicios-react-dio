b4d13b6b6d4bfc2feb26e3ba9e6afac8dc3d0d91
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-11-26 12:43:35 -0300

    Tela Login

--- react-cli-navegacao/src/pages/login/index.jsx
+++ react-cli-navegacao/src/pages/login/index.jsx
@@ -1,8 +1,37 @@
-import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { MdEmail, MdLock } from "react-icons/md";
+
+import { Container, Title, Wrapper, Row, Column, CriarText, EsqueciText, TitleLogin, SubTitleLogin } from "./styles";
+
+
 const Login = () => {
     return (<>
-        <h1>Login</h1>
-        <Link to="/">Voltar para Home</Link>
+        <Header />
+        <Container>
+            <Column>
+                <Title>
+                   A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rapido nas empresas mais desejadas.
+                </Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                    <TitleLogin>Faça seu cadastro</TitleLogin>
+                    <SubTitleLogin>Faça seu login e make the change._</SubTitleLogin>
+                    <form>
+                        <Input placeholder="E-mail" leftIcon={<MdEmail />}/>
+                        <Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
+                        <Button title="Entrar" variant="secondary" />
+                    </form>
+                    <Row>
+                        <EsqueciText>Esqueci minha senhas</EsqueciText>
+                        <CriarText>Criar conta</CriarText>
+                    </Row>
+                </Wrapper>
+            </Column>
+        </Container>
+       
         </>)
 }
 
