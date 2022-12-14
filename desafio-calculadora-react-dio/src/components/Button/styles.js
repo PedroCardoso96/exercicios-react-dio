f9727fa344576f5665fdd2c402ab50a519b49654
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-12-14 07:50:55 -0300

    Calculadora
    Terminando a calculadora

--- desafio-calculadora-react-dio/src/components/Button/styles.js
+++ desafio-calculadora-react-dio/src/components/Button/styles.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
 export const ButtonContainer = styled.div`
-    padding: 10px;
+    padding: 20px;
     border: 1px solid #cdcdcd;
     background-color: #00aaff;
     color: #ffffff;
     font-size: 24px;
     font-weight: 700;
+    flex: 1;
+    text-align: center;
+
+    &:hover {
+        opacity: 0.75;
+    }
 `
--- desafio-calculadora-react-dio/src/styles.js
+++ desafio-calculadora-react-dio/src/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    height:100vh;
+    height: 100vh;
     background-color: #cacaca;
 
     display: flex;
@@ -12,6 +12,18 @@ export const Container = styled.div`
 
 export const Content =styled.div`
     background-color: #ffffff;
-    width: 80%;
-    min-height: 350px;
+    width: 50%;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
